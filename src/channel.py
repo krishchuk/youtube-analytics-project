df10620c695de65f8a720c8eedb5c8f4ac12cabf
denis krishchuk <ms.den.best@mail.ru>
2023-12-06 00:48:42 +0500

    hw1 - made class Channel + requirements.txt

--- src/channel.py
+++ src/channel.py
@@ -1,12 +1,20 @@
+import json
+import os
+
+from googleapiclient.discovery import build
 
 
 class Channel:
     """Класс для ютуб-канала"""
 
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "C:/manifest-ocean-407215-51b1854c7f81.json"
+    api_key: str = os.getenv('YOUTUBE_API_KEY')
+    youtube = build('youtube', 'v3', developerKey=api_key)
+
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        pass
+        self.channel = self.youtube.channels().list(id=channel_id, part='snippet,statistics').execute()
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        pass
+        print(json.dumps(self.channel, indent=2, ensure_ascii=False))
