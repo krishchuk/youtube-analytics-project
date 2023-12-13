08e6ef1e11a79770b9e3d42f9d84fcbd445bbca3
denis krishchuk <ms.den.best@mail.ru>
2023-12-13 21:57:39 +0500

    modified get_service

--- src/channel.py
+++ src/channel.py
@@ -9,11 +9,10 @@ class Channel:
 
     os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "C:/manifest-ocean-407215-51b1854c7f81.json"
     api_key: str = os.getenv('YOUTUBE_API_KEY')
-    youtube = build('youtube', 'v3', developerKey=api_key)
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        self.channel = self.youtube.channels().list(id=channel_id, part='snippet,statistics').execute()
+        self.channel = self.get_service().channels().list(id=channel_id, part='snippet,statistics').execute()
         self.__channel_id = channel_id
         self.title = self.channel['items'][0]['snippet']['title']
         self.description = self.channel['items'][0]['snippet']['description']
@@ -45,7 +44,7 @@ class Channel:
 
     @classmethod
     def get_service(cls):
-        return Channel.youtube
+        return build('youtube', 'v3', developerKey=cls.api_key)
 
 
 # ch = Channel('UC-OVMPlMA3-YCIeg4z5z23A')
