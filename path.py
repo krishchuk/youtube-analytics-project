2b91b6deb727d9c389aa8b21e6e43837456fef3e
denis krishchuk <ms.den.best@mail.ru>
2023-12-19 23:43:02 +0500

    hw4 - rename json file to ".env"

--- path.py
+++ path.py
@@ -3,5 +3,5 @@ from pathlib import Path
 
 
 def get_path() -> str:
-    path_file = Path(pathlib.Path.cwd().parent, "manifest-ocean-407215-51b1854c7f81.json")
+    path_file = Path(pathlib.Path.cwd().parent, ".env")
     return str(path_file)
